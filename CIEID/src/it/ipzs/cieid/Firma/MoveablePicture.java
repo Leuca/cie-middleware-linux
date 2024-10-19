9b529fbe7c00a3b11755865c45d9adf9dd51cad9
Luca Magrone <luca@magrone.cc>
2024-10-19 11:23:42 +0200

    CIEID: PADES: Improve graphical signature preview and make a smaller graphical signature
    Make the background of the signature transparent.
    Shrink the signature.
    Make the border dashed.
    
    Signed-off-by: Luca Magrone <luca@magrone.cc>

--- CIEID/src/it/ipzs/cieid/Firma/MoveablePicture.java
+++ CIEID/src/it/ipzs/cieid/Firma/MoveablePicture.java
@@ -21,13 +21,13 @@ public class MoveablePicture extends JPanel implements MouseListener, MouseMotio
 	
 	public MoveablePicture(String signFilePath) {
 		// TODO Auto-generated constructor stub
-		this.setSize(90, 25);
-		this.setBackground(Color.white);
-		this.setBorder(BorderFactory.createLineBorder(Color.black));
+		this.setSize(50, 25);
+		this.setBackground(new Color(255, 255, 255, 0));
+		this.setBorder(BorderFactory.createDashedBorder(Color.black));
 		try {
 			Image img;
 			img = ImageIO.read(new File(signFilePath));
-			JLabel imgLabel = new JLabel(new ImageIcon(img.getScaledInstance(this.getWidth()-5, this.getHeight()-10, Image.SCALE_SMOOTH)));
+			JLabel imgLabel = new JLabel(new ImageIcon(img.getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_SMOOTH)));
 			this.add(imgLabel);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
