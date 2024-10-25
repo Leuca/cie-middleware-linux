1b2cd1b25954ec46256a043f00f099e753b1ee51
Luca Magrone <luca@magrone.cc>
2024-10-25 16:00:39 +0200

    build.sh: Avoid restoring user changes with git restore
    Signed-off-by: Luca Magrone <luca@magrone.cc>

--- build.sh
+++ build.sh
@@ -4,42 +4,52 @@
 SDK_PATH=./cie_sign_sdk
 CIE_PATH=./cie-pkcs11
 LIBCIE_PATH=./libcie-pkcs11
+SDK_REMOVE_LIST=(
+	$SDK_PATH/CMakeLists.txt
+	$SDK_PATH/src/Util/UUC*
+	)
+CIE_REMOVE_LIST=(
+	$CIE_PATH/keys.h
+	$CIE_PATH/*.a
+	$CIE_PATH/Util/funccallinfo.cpp
+	)
+REMOVE_LIST=(${SDK_REMOVE_LIST[@]} ${CIE_REMOVE_LIST[@]})
 GIT=$(which git)
 CMAKE=$(which cmake)
 
 # Remove pre-compiled static-libs
 if [ -d $SDK_PATH/Dependencies ]; then
 	rm -rf $SDK_PATH/Dependencies
+	REMOVE_LIST+=($SDK_PATH/Dependencies)
 fi
 
 # Remove CryptoPP headers
 if [ -d $SDK_PATH/src/cryptopp ]; then
 	rm -rf $SDK_PATH/src/cryptopp
+	REMOVE_LIST+=($SDK_PATH/src/cryptopp)
 fi
 if [ -d $CIE_PATH/Cryptopp ]; then
 	rm -rf $CIE_PATH/Cryptopp
+	REMOVE_LIST+=($CIE_PATH/Cryptopp)
 fi
 
 # Merge cie_sign_sdk source with cie-pkcs11 source
 mkdir -p $LIBCIE_PATH
-rm -f $SDK_PATH/CMakeLists.txt
-rm -f $SDK_PATH/README.mk
-rm -f $SDK_PATH/src/Util/UUC*
+rm -f ${SDK_REMOVE_LIST[@]}
 cp -r $SDK_PATH/* $LIBCIE_PATH/
 cp $CIE_PATH/keys.h $LIBCIE_PATH/include/
-rm -f $CIE_PATH/keys.h
-rm -f $CIE_PATH/*.a
-rm -f $CIE_PATH/Util/funccallinfo.cpp
+rm -f ${CIE_REMOVE_LIST[@]}
 cp -f $CIE_PATH/Util/UUCStringTable.cpp $LIBCIE_PATH/src/
 cp -f $CIE_PATH/Util/UUCStringTable.h $LIBCIE_PATH/include/
 cp -f $CIE_PATH/Util/UUCHashtable.hpp $LIBCIE_PATH/include/
 rm -f $CIE_PATH/Util/UUC*
+REMOVE_LIST+=($CIE_PATH/Util/UUC*)
 cp -rf $CIE_PATH/* $LIBCIE_PATH/src/
 rm -f $LIBCIE_PATH/src/Sign/definitions.h
 
 # Restore git repository
 if [ "$GIT" != "" ]; then
-	$GIT restore .
+	$GIT restore ${REMOVE_LIST[@]}
 else
 	echo "Warning: git not found"
 fi
